f1660b99f0ae942950edcdd1d47e1849d3609c47
PHYO THI HA <phyothiha97531@gmail.com>
2024-12-23 00:41:30 +0630

    wada

--- src/app/state/[id]/page.tsx
+++ src/app/state/[id]/page.tsx
@@ -1,5 +1,4 @@
 import getCountryState from "@/app/getCountryState";
-import Image from "next/image";
 
 export default async function Page ({
     searchParams,
@@ -7,21 +6,8 @@ export default async function Page ({
     searchParams: Promise<{ [key: string]: string | string[] | undefined }>
   }) {
     const {id} = await searchParams
-    const state = await getCountryState(id);
-
-    if (!id) {
-      return <div>No Id</div>
-    }
-
-    if (!state) {
-      return <div>Can not get state</div>
-    }
 
     return (
-        <div className="h-[70vh] px-3 flex flex-col gap-2">
-            <Image src={state.url} alt="myanmar people" width={150} height={150} />
-            <h1>{state.name}</h1>
-            <p>{state.description}</p>
-        </div>
+        <div>{id}</div>
     )
 };
