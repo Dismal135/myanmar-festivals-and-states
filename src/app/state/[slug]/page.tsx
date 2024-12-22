504ae7bd13ef92d0c68fd43b26d376750237def8
PHYO THI HA <phyothiha97531@gmail.com>
2024-12-23 01:45:14 +0630

    awda

--- src/app/state/[slug]/page.tsx
+++ src/app/state/[slug]/page.tsx
@@ -1,10 +1,13 @@
 export default async function Page ({
   params,
+  searchParams,
 }: {
   params: Promise<{ slug: string }>
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
 }) {
   const {slug} = await params
+  const {id} = await searchParams
     return (
-        <div> quey {slug}</div>
+        <div> param {slug} query {id} </div>
     )
 };
