a1bd1326e3a487e339c793dad0ea99df05718cf7
PHYO THI HA <phyothiha97531@gmail.com>
2024-12-22 23:32:59 +0630

    findingerror

--- src/app/state/[id]/page.tsx
+++ src/app/state/[id]/page.tsx
@@ -9,13 +9,9 @@ export default async function Page ({
     const id = (await searchParams).id
     const state = await getCountryState(id);
 
-    if (!state) {
-      return <div>Network Error!</div>
-    }
-
     return (
         <div className="h-[70vh] px-3 flex flex-col gap-2">
-            <Image src={state.url} alt="myanmar people" width={150} height={150} />
+            {/* <Image src={state.url} alt="myanmar people" width={150} height={150} /> */}
             <h1>{state.name}</h1>
             <p>{state.description}</p>
         </div>
